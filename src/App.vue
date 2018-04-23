b1d8508dc12fbd6024903ef1d2d3e3a907d8b2da
heroMRQ <463363460@qq.com>
2018-04-23 23:30:06 +0800

    找app

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,15 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+      <div class="header">头部</div>
+      <div class="body">身体</div>
+      <div class="footer">尾部</div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-};
+// export default { };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
