d6d6178e0ed7ce34b0a5718c3dabdb8b180fa032
heroMRQ <463363460@qq.com>
2018-04-24 23:28:48 +0800

    开始搭建路由

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,38 @@
 <template>
   <div id="app">
-      <div class="header">头部</div>
-      <div class="body">身体</div>
-      <div class="footer">尾部</div>
+      <v-header></v-header>
+      <div class="tab">
+        <div class="tab-item">
+          <a v-link="{path:'/goods'}">商品</a>
+        </div>
+        <div class="tab-item">
+          <a v-link="{path:'/ratings'}">用户</a>
+        </div>
+        <div class="tab-item">
+          <a v-link="{path:'/seller'}">商家</a>
+        </div>
+      </div>
+     <router-view></router-view>
   </div>
 </template>
 
 <script>
-// export default { };
+import header from './components/header/header';
+export default {
+  components: {
+    'v-header': header
+  }
+ };
 </script>
 
-<style>
-
+<style lang="stylus" rel="stylesheet/stylus">
+  #app
+    .tab
+      display:flex
+      width:100%
+      height:40px
+      line-height: 40px
+      .tab-item
+        flex:1
+        text-align:center
 </style>
