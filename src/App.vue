20bfb697aea15ebcd0959122aa4cf568434b76b0
maruiqian <maruiqian@btzh.cn>
2018-04-25 14:40:53 +0800

    styl出错

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,13 @@
       <v-header></v-header>
       <div class="tab">
         <div class="tab-item">
-          <router-link to="{path:'/goods'}">商品</router-link>
+          <router-link to="/goods">商品</router-link>
         </div>
         <div class="tab-item">
-          <router-link to="{path:'/ratings'}">用户</router-link>
+          <router-link to="/ratings">用户</router-link>
         </div>
         <div class="tab-item">
-          <router-link to="{path:'/seller'}">商家</router-link>
+          <router-link to="/seller">商家</router-link>
         </div>
       </div>
       <router-view></router-view>
@@ -28,11 +28,18 @@ export default {
 <style lang="stylus" rel="stylesheet/stylus">
   #app
     .tab
-      display:flex
-      width:100%
-      height:40px
+      display: flex
+      width: 100%
+      height: 40px
       line-height: 40px
+      border-bottom: 1px solid rgba(7, 17, 27, 0.1);
       .tab-item
-        flex:1
-        text-align:center
+        flex: 1
+        text-align: center
+        & > a
+          display: block
+          font-size: 14px
+          color: rgb(77, 85 ,93)
+          &.router-link-active
+            color: rgb(240, 20 ,20)
 </style>
